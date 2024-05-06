312d47e3e4ef8aaed6dd129b3d5ad19e9cab2b86
李建维 <lijianwei@hualala.com>
2024-05-06 12:38:26 +0800

    b

--- utils/excel/excel.go
+++ utils/excel/excel.go
@@ -242,8 +242,8 @@ func CreateFile(m ncr.Manage) error {
 	//问题4
 	ControlsWriteExcel(f, "A"+intTostr(x), "AU"+intTostr(y), "Root cause analysis Ishikawa 原因分析鱼骨图", SetStyle(f,
 		20, "Frutiger LT 55 Roman", black, "left", "center", true))
-	x += 36
-	y += 36
+	x += 6
+	y += 6
 	// 去掉 Base64 编码的头部
 	imgBase64 := strings.Replace(m.A3_Img_Base64, "data:image/png;base64,", "", 1)
 
